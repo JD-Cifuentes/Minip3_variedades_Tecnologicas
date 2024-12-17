3c4684341f30e6ad21908f613ec3358d84cccbc7
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2024-12-17 12:15:48 -0500

    Se genera visual para cierre del día

--- src/Visuales/CierreDia.java
+++ src/Visuales/CierreDia.java
@@ -4,10 +4,22 @@ import Entidades.Negocio;
 import Utils.AccessPanel;
 
 import javax.swing.*;
-import java.io.Serializable;
 
 public class CierreDia implements AccessPanel {
     private JPanel CierreDia_BG;
+    private JButton bttn_generarCierreDia;
+    private JLabel lbl_totalDineroRecogido;
+    private JLabel lbl_impresoraConMasConsumo;
+    private JLabel lbl_totalGanacia;
+    private JLabel lbl_gastosDeProduccion;
+    private JLabel lbl_servicioConMayorGanacia;
+    private JLabel lbl_impresoraConMenorGanacia;
+    private JTextField txtReadOnly_totalDineroRecogido;
+    private JTextField txtReadOnly_gananciaObtenida;
+    private JTextField txtReadOnly_gastosProduccion;
+    private JTextField txtReadOnly_servicioConMayorGanancia;
+    private JTextField txtReadOnly_impresoraConmenorGanacia;
+    private JTextField txtReadOnly_impresoraMayorConsumoTinta;
 
     public CierreDia(Negocio local) {
     }
